7f67e41542e3d5ddd93a030d3f064e8366dd329c
Ramandeep Kaur <rkaur@ncsu.edu>
2019-10-19 00:54:47 -0500

    Use case partial code added

--- Milestone2/bot.js
+++ Milestone2/bot.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const SlackBot = require("slackbots");
 
+//List containing all image types
+const image_types = ["jpeg" , "jpg" , "jpe" , "jfif" , "jif" , "jfi" , "png" , "svg" , "webp" , "tiff" , "tif" , "psd" , "raw" , "arw" , "cr2" , "nrw" , "k25" , "bmp"] 
+
 const bot = new SlackBot({
 	token: process.env.SLACK_BOT_TOKEN,
 	name: 'secbot'});
@@ -18,8 +21,25 @@ bot.on('error', (err) => {console.log(err)});
 
 //Message Handler
 bot.on('message', (data) => {
-	if(data.type !== 'message') {
-		return; 
+	if("files" in data ) {
+		//Use Case 1 ( Image checking)
+		if ( image_types.includes(data["files"][0]['filetype'].toLowerCase()) ) { 
+			if ( (data["files"][0]['name']).match(/corrupted/i) )
+				bot.postMessageToChannel('general', "The image is corrupted");
+		}
+		//Use Case 2 ( File checking )
+		else {
+			if ( (data["files"][0]['name']).match(/corrupted/i) ) {
+				bot.postMessageToChannel('general', "The file is corrupted");
+			}
+		}
 	}
-	//Function to handle data and 3 use cases	
+	//Use Case 3 ( Report requesting )
+	else {
+		if ( (data["type"] == "message") & ("client_msg_id" in data) ) {
+			if (data["text"].match(/request/i) )  {
+				bot.postMessageToChannel('general', "The report is here");
+		}
+		}
+	}
 });
