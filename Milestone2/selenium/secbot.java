328b05cabd9c94f70596ced41c79ff4a0f7ce2b6
Ramandeep Kaur <rkaur@ncsu.edu>
2019-10-21 19:55:17 -0400

    USE CASE 2

--- Milestone2/selenium/secbot.java
+++ Milestone2/selenium/secbot.java
@@ -8,83 +8,193 @@ import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class secbot 
-{	
-	public static WebDriver driver = null;
-	public static void main(String[] args) throws InterruptedException 
-	{
-		//Setting the location of chromedriver
-		System.setProperty("webdriver.chrome.driver", System.getenv("CHROMEDRIVER"));
-		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		
-		//Setting the URL
-		driver.get(System.getenv("SLACK_WORKSPACE_URL"));
-		driver.manage().window().maximize();
-		
-		// Wait for the sign in button
-        	WebDriverWait wait = new WebDriverWait(driver, 30);
-        	wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("signin_btn")));
+public class secbot
+{
+        public static WebDriver driver = null;
+        public static void main(String[] args) throws InterruptedException
+        {
+                //Setting the location of chromedriver
+                System.setProperty("webdriver.chrome.driver", System.getenv("CHROMEDRIVER"));
+                driver = new ChromeDriver();
+                driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        	WebElement email = driver.findElement(By.id("email"));
-        	WebElement pw = driver.findElement(By.id("password"));
+                //Setting the URL
+                driver.get(System.getenv("SLACK_WORKSPACE_URL"));
+                driver.manage().window().maximize();
 
-        	//Setting the email and password
-        	email.sendKeys(System.getenv("SELENIUM_USER_EMAIL"));
-       	 	pw.sendKeys(System.getenv("SELENIUM_USER_PASSWORD"));
+                // Wait for the sign in button
+                WebDriverWait wait = new WebDriverWait(driver, 30);
+                wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("signin_btn")));
 
-        	// Click the sign-in button
-        	WebElement signin = driver.findElement(By.id("signin_btn"));
-        	signin.click();
+                WebElement email = driver.findElement(By.id("email"));
+                WebElement pw = driver.findElement(By.id("password"));
 
-        	// Go to #general channel
-        	wait.until(ExpectedConditions.titleContains("general"));
+                //Setting the email and password
+                email.sendKeys(System.getenv("SELENIUM_USER_EMAIL"));
+                pw.sendKeys(System.getenv("SELENIUM_USER_PASSWORD"));
 
-        	// Welcome message
-        	WebElement messageBot = driver.findElement(By.xpath("//*[@id=\"undefined\"]/p"));
+                // Click the sign-in button
+                WebElement signin = driver.findElement(By.id("signin_btn"));
+                signin.click();
 
-    		messageBot.sendKeys("Hello Dunder Mifflin, from Creed");
-    		messageBot.sendKeys(Keys.RETURN);
-        
-        	wait.withTimeout(3, TimeUnit.SECONDS).ignoring(StaleElementReferenceException.class);
-        
-        	//********************************* USE CASE 1 ***************************************//
-        
-        	// Locate the Attach button
-        	messageBot = driver.findElement(By.xpath("/html/body/div[2]/div/div/div[4]/div/div/footer/div/div/div[1]/button"));
-        	messageBot.click();
-        
-        	// Click on the Google Drive button
-        	WebElement upload = driver.findElement(By.xpath("//*[text()='Google Drive']"));
-        	upload.click();
-        
-        	// Wait for 5 seconds for the driver to load all window handles
-        	Thread.sleep(5000);
-        
-        	// Logic to switch to a new window handle
-        	java.util.Set<String> allHandles = driver.getWindowHandles();
-        	String currentHandle = driver.getWindowHandle();
-        	allHandles.remove(currentHandle);
-        	String nextHandle=allHandles.iterator().next();
-        	String newHandle = "";
-        	if(currentHandle != nextHandle)
-        	{
-        		newHandle = nextHandle;
-        		driver.switchTo().window(newHandle);
-        		driver.manage().window().maximize();
-        	
-        		// The window handle loaded is a frame
-    	    		driver.switchTo().frame(1);
-        		WebElement select_image = driver.findElement(By.xpath("//*[text()='abcdef corrupted.JPG']"));
-            		select_image.click();
-            		WebElement click_select = driver.findElement(By.xpath("//*[text()='Select']"));
-            		click_select.click();
-        	}      
-        
-        	driver.close();
-        	driver.quit();
-	}
+                // Go to #general channel
+                wait.until(ExpectedConditions.titleContains("general"));
+
+                // Welcome message
+                WebElement messageBot = driver.findElement(By.xpath("//*[@id=\"undefined\"]/p"));
+
+                messageBot.sendKeys("Hello Dunder Mifflin, from Creed");
+                messageBot.sendKeys(Keys.RETURN);
+
+                wait.withTimeout(3, TimeUnit.SECONDS).ignoring(StaleElementReferenceException.class);
+
+                //********************************* USE CASE 1 CORRUPTED IMAGE ***************************************//
+
+                // Locate the Attach button
+                messageBot = driver.findElement(By.xpath("/html/body/div[2]/div/div/div[4]/div/div/footer/div/div/div[1]/button"));
+                messageBot.click();                
+    
+                // Click on the Google Drive button
+                WebElement upload = driver.findElement(By.xpath("//*[text()='Google Drive']"));
+                upload.click();
+
+                // Wait for 5 seconds for the driver to load all window handles
+                Thread.sleep(5000);
+
+                // Logic to switch to a new window handle
+                java.util.Set<String> allHandles = driver.getWindowHandles();
+                String currentHandle = driver.getWindowHandle();
+                allHandles.remove(currentHandle);
+                String nextHandle=allHandles.iterator().next();
+                String newHandle = "";
+                if(currentHandle != nextHandle)
+                {
+                        newHandle = nextHandle;
+                        driver.switchTo().window(newHandle);
+                        driver.manage().window().maximize();
+
+                        // The window handle loaded is a frame
+                        driver.switchTo().frame(1);
+                        WebElement select_image = driver.findElement(By.xpath("//*[text()='abcdef corrupted.JPG']"));
+                        select_image.click();
+                        WebElement click_select = driver.findElement(By.xpath("//*[text()='Select']"));
+                        click_select.click();
+                }
+                driver.switchTo().window(currentHandle);
+                //********************************* USE CASE 1 APPROPRIATE IMAGE***************************************//
+                
+                // Locate the Attach button
+                messageBot = driver.findElement(By.xpath("/html/body/div[2]/div/div/div[4]/div/div/footer/div/div/div[1]/button"));
+                messageBot.click();
+                
+    
+                // Click on the Google Drive button
+                upload = driver.findElement(By.xpath("//*[text()='Google Drive']"));
+                upload.click();
+
+                // Wait for 5 seconds for the driver to load all window handles
+                Thread.sleep(5000);
+
+                // Logic to switch to a new window handle
+                allHandles = driver.getWindowHandles();
+                currentHandle = driver.getWindowHandle();
+                allHandles.remove(currentHandle);
+                nextHandle=allHandles.iterator().next();
+                newHandle = "";
+                if(currentHandle != nextHandle)
+                {
+                        newHandle = nextHandle;
+                        driver.switchTo().window(newHandle);
+                        driver.manage().window().maximize();
+
+                        // The window handle loaded is a frame
+                        driver.switchTo().frame(1);
+                        WebElement select_image = driver.findElement(By.xpath("//*[text()='abcdef.JPG']"));
+                        select_image.click();
+                        WebElement click_select = driver.findElement(By.xpath("//*[text()='Select']"));
+                        click_select.click();
+                }
+                driver.switchTo().window(currentHandle);
+                
+                //********************************* USE CASE 2 CORRUPTED FILE***************************************//
+                
+                // Locate the Attach button
+                messageBot = driver.findElement(By.xpath("/html/body/div[2]/div/div/div[4]/div/div/footer/div/div/div[1]/button"));
+                messageBot.click();
+                
+    
+                // Click on the Google Drive button
+                upload = driver.findElement(By.xpath("//*[text()='Google Drive']"));
+                upload.click();
+
+                // Wait for 5 seconds for the driver to load all window handles
+                Thread.sleep(5000);
+
+                // Logic to switch to a new window handle
+                allHandles = driver.getWindowHandles();
+                currentHandle = driver.getWindowHandle();
+                allHandles.remove(currentHandle);
+                nextHandle=allHandles.iterator().next();
+                newHandle = "";
+                if(currentHandle != nextHandle)
+                {
+                        newHandle = nextHandle;
+                        driver.switchTo().window(newHandle);
+                        driver.manage().window().maximize();
+
+                        // The window handle loaded is a frame
+                        driver.switchTo().frame(1);
+                        WebElement select_image = driver.findElement(By.xpath("//*[text()='abcdef corrupted.pdf']"));
+                        select_image.click();
+                        WebElement click_select = driver.findElement(By.xpath("//*[text()='Select']"));
+                        click_select.click();
+                }
+                driver.switchTo().window(currentHandle);
+                
+                //********************************* USE CASE 2 APPROPRIATE FILE***************************************//
+                
+                // Locate the Attach button
+                messageBot = driver.findElement(By.xpath("/html/body/div[2]/div/div/div[4]/div/div/footer/div/div/div[1]/button"));
+                messageBot.click();
+                
+    
+                // Click on the Google Drive button
+                upload = driver.findElement(By.xpath("//*[text()='Google Drive']"));
+                upload.click();
+
+                // Wait for 5 seconds for the driver to load all window handles
+                Thread.sleep(5000);
+
+                // Logic to switch to a new window handle
+                allHandles = driver.getWindowHandles();
+                currentHandle = driver.getWindowHandle();
+                allHandles.remove(currentHandle);
+                nextHandle=allHandles.iterator().next();
+                newHandle = "";
+                if(currentHandle != nextHandle)
+                {
+                        newHandle = nextHandle;
+                        driver.switchTo().window(newHandle);
+                        driver.manage().window().maximize();
+
+                        // The window handle loaded is a frame
+                        driver.switchTo().frame(1);
+                        WebElement select_image = driver.findElement(By.xpath("//*[text()='abcdef.pdf']"));
+                        select_image.click();
+                        WebElement click_select = driver.findElement(By.xpath("//*[text()='Select']"));
+                        click_select.click();
+                }
+                
+                
+                
+                driver.close();
+                driver.quit();
+                
+                
+                
+        }
 }
